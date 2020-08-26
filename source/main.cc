00e200ccf16765ab675b13d96a43bd1181cfe6d3
Carlos Chinchilla <cachinchilla@google.com>
2020-08-26 10:35:51 -0700

    Added example assert usage
    Added sample asserts to main app and dependency to pw_assert with the
    default toolchain assert backend.
    
    Change-Id: I164cc648e8defa3b1277b1c7814404c0b7926650
    Reviewed-on: https://pigweed-review.googlesource.com/c/pigweed/sample_project/+/16820
    Reviewed-by: Keir Mierle <keir@google.com>
    Commit-Queue: Carlos Chinchilla <cachinchilla@google.com>

--- source/main.cc
+++ source/main.cc
@@ -1,16 +1,23 @@
 #include "simple_counter/simple_counter.h"
 
+#include "pw_assert/assert.h"
 #include "pw_log/log.h"
 
 int main() {
   // Use sample Module
   sample::SimpleCounter counter(3);
   counter.Add(-1);
+  // Sample assert. Adding a message is optional.
+  PW_CHECK_INT_EQ(counter.GetCount(), 2);
   counter.Add(10);
 
-  if (counter.GetCount() != 12) {
-    return 1;
-  }
+  // Chance this value to see the assert below crash the app.
+  const int expected_count = 12;
+
+  // Use sample assert with a message.
+  PW_CHECK_INT_EQ(counter.GetCount(), expected_count,
+                  "Counter value %d != %d as expected",
+                  counter.GetCount(), expected_count);
 
   PW_LOG_INFO("Hello World!");
 
