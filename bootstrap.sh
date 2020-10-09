cf4929166df8eca79584f54ed4a7724563cc37ff
Anthony DiGirolamo <tonymd@google.com>
2020-10-08 18:19:22 -0700

    Fix source activate.sh
    Running `source activate.sh` always invokes pigweed bootstrap. This
    change will source activate.sh or bootstrap.sh depending on which
    script the user sources. This should speedup subsequent activate.sh
    source invokations.
    
    Change-Id: I35110d2345cda98e45d7a42bdd16f1474f261e1d
    Reviewed-on: https://pigweed-review.googlesource.com/c/pigweed/sample_project/+/20221
    Reviewed-by: Carlos Chinchilla <cachinchilla@google.com>
    Commit-Queue: Anthony DiGirolamo <tonymd@google.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -21,10 +21,29 @@ _project_abspath () {
   python -c "import os.path; print(os.path.abspath('$@'))"
 }
 
-PW_BOOTSTRAP_SCRIPT=$(_project_abspath)/third_party/pigweed/bootstrap.sh
+# Default to third_party/pigweed/bootstrap.sh
+PW_SETUP_SCRIPT_PATH=$(_project_abspath third_party/pigweed/bootstrap.sh)
 
-if [ ! -e ${PW_BOOTSTRAP_SCRIPT} ]; then
-  echo "Error: ${PW_BOOTSTRAP_SCRIPT} not found."
+# Set third_party/pigweed/{activate.sh,bootstrap.sh}
+# depending on which name was sourced.
+
+# Shell: bash.
+if test -n "$BASH"; then
+  PW_SETUP_SCRIPT_PATH="$(_project_abspath third_party/pigweed/"$BASH_SOURCE")"
+# Shell: zsh.
+elif test -n "$ZSH_NAME"; then
+  PW_SETUP_SCRIPT_PATH="$(_project_abspath third_party/pigweed/"${(%):-%N}")"
+# Shell: dash.
+elif test ${0##*/} = dash; then
+  PW_SETUP_SCRIPT_PATH="$(_project_abspath \
+    third_party/pigweed/"$(lsof -p $$ -Fn0 | tail -1 | sed 's#^[^/]*##;')")"
+# If everything else fails, try $0. It could work.
+else
+  PW_SETUP_SCRIPT_PATH="$(_project_abspath third_party/pigweed/"$0")"
+fi
+
+if [ ! -e ${PW_SETUP_SCRIPT_PATH} ]; then
+  echo "Error: ${PW_SETUP_SCRIPT_PATH} not found."
   echo "Did you forget to initialize the git submodules?"
   echo "To setup the git submodules run:"
   echo "  git submodule init"
@@ -34,6 +53,6 @@ fi
 
 export SAMPLE_PROJECT_ROOT=$(_project_abspath)
 
-source $PW_BOOTSTRAP_SCRIPT
+source $PW_SETUP_SCRIPT_PATH
 
 # Add user-defined environment setup here.
