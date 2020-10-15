4243f01bfb4e5e4bf73072a9c91970674f212cd6
Rob Mohr <mohrr@google.com>
2020-10-15 10:53:46 -0700

    Fix bootstrap after pwrev/19380
    Change-Id: Ifda7cdd7a74bf67f844b7736ca9388be7a6eecbb
    Bug: 274
    Reviewed-on: https://pigweed-review.googlesource.com/c/pigweed/sample_project/+/21340
    Reviewed-by: Carlos Chinchilla <cachinchilla@google.com>
    Commit-Queue: Rob Mohr <mohrr@google.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,47 +12,120 @@
 # License for the specific language governing permissions and limitations under
 # the License.
 
-# Set your project's banner and color.
-export PW_BRANDING_BANNER="banner.txt"
-export PW_BRANDING_BANNER_COLOR=cyan
+# This script must be sourced, not executed.
+#
+# Create a new environment:
+#
+#   source bootstrap.sh
+#
+# Activate an existing environment:
+#
+#   source activate.sh
 
-# First, activate the Pigweed development environment.
-_project_abspath () {
+_bootstrap_abspath () {
   python -c "import os.path; print(os.path.abspath('$@'))"
 }
 
-# Default to third_party/pigweed/bootstrap.sh
-PW_SETUP_SCRIPT_PATH=$(_project_abspath third_party/pigweed/bootstrap.sh)
-
-# Set third_party/pigweed/{activate.sh,bootstrap.sh}
-# depending on which name was sourced.
-
+# Users are not expected to set PW_CHECKOUT_ROOT, it's only used because it
+# seems to be impossible to reliably determine the path to a sourced file in
+# dash when sourced from a dash script instead of a dash interactive prompt.
+# To reinforce that users should not be using PW_CHECKOUT_ROOT, it is cleared
+# here after it is used, and other pw tools will complain if they see that
+# variable set.
+# TODO(mohrr) find out a way to do this without PW_CHECKOUT_ROOT.
+if test -n "$PW_CHECKOUT_ROOT"; then
+  _BOOTSTRAP_PATH="$(_bootstrap_abspath "$PW_CHECKOUT_ROOT/bootstrap.sh")"
+  unset PW_CHECKOUT_ROOT
 # Shell: bash.
-if test -n "$BASH"; then
-  PW_SETUP_SCRIPT_PATH="$(_project_abspath third_party/pigweed/"$BASH_SOURCE")"
+elif test -n "$BASH"; then
+  _BOOTSTRAP_PATH="$(_bootstrap_abspath "$BASH_SOURCE")"
 # Shell: zsh.
 elif test -n "$ZSH_NAME"; then
-  PW_SETUP_SCRIPT_PATH="$(_project_abspath third_party/pigweed/"${(%):-%N}")"
+  _BOOTSTRAP_PATH="$(_bootstrap_abspath "${(%):-%N}")"
 # Shell: dash.
 elif test ${0##*/} = dash; then
-  PW_SETUP_SCRIPT_PATH="$(_project_abspath \
-    third_party/pigweed/"$(lsof -p $$ -Fn0 | tail -1 | sed 's#^[^/]*##;')")"
+  _BOOTSTRAP_PATH="$(_bootstrap_abspath \
+    "$(lsof -p $$ -Fn0 | tail -1 | sed 's#^[^/]*##;')")"
 # If everything else fails, try $0. It could work.
 else
-  PW_SETUP_SCRIPT_PATH="$(_project_abspath third_party/pigweed/"$0")"
+  _BOOTSTRAP_PATH="$(_bootstrap_abspath "$0")"
 fi
 
-if [ ! -e ${PW_SETUP_SCRIPT_PATH} ]; then
-  echo "Error: ${PW_SETUP_SCRIPT_PATH} not found."
+# Check if this file is being executed or sourced.
+_pw_sourced=0
+if [ -n "$SWARMING_BOT_ID" ]; then
+  # If set we're running on swarming and don't need this check.
+  _pw_sourced=1
+elif [ -n "$ZSH_EVAL_CONTEXT" ]; then
+  case $ZSH_EVAL_CONTEXT in *:file) _pw_sourced=1;; esac
+elif [ -n "$KSH_VERSION" ]; then
+  [ "$(cd $(dirname -- $0) && pwd -P)/$(basename -- $0)" != \
+    "$(cd $(dirname -- ${.sh.file}) && pwd -P)/$(basename -- ${.sh.file})" ] \
+    && _pw_sourced=1
+elif [ -n "$BASH_VERSION" ]; then
+  (return 0 2>/dev/null) && _pw_sourced=1
+else  # All other shells: examine $0 for known shell binary filenames
+  # Detects `sh` and `dash`; add additional shell filenames as needed.
+  case ${0##*/} in sh|dash) _pw_sourced=1;; esac
+fi
+
+# The SAMPLE_PROJECT_ROOT variable points to the root of the sample_project repo structure.
+SAMPLE_PROJECT_ROOT="$(_bootstrap_abspath "$(dirname "$_BOOTSTRAP_PATH")")"
+export SAMPLE_PROJECT_ROOT
+
+# Set PW_ROOT based on where the Pigweed submodule is in your repository.
+PW_ROOT="$SAMPLE_PROJECT_ROOT/third_party/pigweed"
+export PW_ROOT
+
+_util_sh="$PW_ROOT/pw_env_setup/util.sh"
+
+# Double-check that the Pigweed submodule has been checked out.
+if [ ! -e "$_util_sh" ]; then
+  echo "Error: $_util_sh not found."
   echo "Did you forget to initialize the git submodules?"
   echo "To setup the git submodules run:"
-  echo "  git submodule init"
-  echo "  git submodule update"
+  echo "  git submodule update --init"
   return
 fi
 
-export SAMPLE_PROJECT_ROOT=$(_project_abspath)
+. "$_util_sh"
 
-source $PW_SETUP_SCRIPT_PATH
+pw_eval_sourced "$_pw_sourced"
+pw_check_root "$PW_ROOT"
+_PW_ACTUAL_ENVIRONMENT_ROOT="$(pw_get_env_root)"
+SETUP_SH="$_PW_ACTUAL_ENVIRONMENT_ROOT/activate.sh"
 
-# Add user-defined environment setup here.
+# Set your project's banner and color.
+export PW_BRANDING_BANNER="$SAMPLE_PROJECT_ROOT/banner.txt"
+export PW_BRANDING_BANNER_COLOR=cyan
+
+sample_project_banner() {
+  # TODO(pwbug/274) Figure out how to use the color here too.
+  cat "$PW_BRANDING_BANNER"
+  echo
+}
+
+PW_BANNER_FUNC="sample_project_banner"
+export PW_BANNER_FUNC
+
+# Run full bootstrap when invoked as bootstrap, or env file is missing/empty.
+if [ "$(basename "$_BOOTSTRAP_PATH")" = "bootstrap.sh" ] || \
+  [ ! -f "$SETUP_SH" ] || \
+  [ ! -s "$SETUP_SH" ]; then
+  # These arguments will be passed through to Pigweed's env_setup.
+  pw_bootstrap --shell-file "$SETUP_SH" --install-dir "$_PW_ACTUAL_ENVIRONMENT_ROOT" --use-pigweed-defaults --virtualenv-setup-py-root "$SAMPLE_PROJECT_ROOT"
+  pw_finalize bootstrap "$SETUP_SH"
+else
+  pw_activate
+  pw_finalize activate "$SETUP_SH"
+fi
+
+# This is where any additional checks about the environment should go.
+
+unset _pw_sourced
+unset _BOOTSTRAP_PATH
+unset SETUP_SH
+unset _bootstrap_abspath
+unset _util_sh
+
+pw_cleanup
