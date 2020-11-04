1c009583d447cf0b313d92e558a73615c49c3d9f
Armando Montanez <amontanez@google.com>
2020-11-03 18:29:15 -0800

    pw_board_led: Add module
    * Adds the STM32F429I-DSIC1 upstream Pigweed target.
    * Adds the pw_board_led facade and two backends.
    * Fixes pw_trace-related warnings.
    
    Adds the pw_board_led facade and backend implementations for Teensy3/4
    and the STM32F429I-DISC1.
    
    Change-Id: I42d8a4142d67e2653217805631fbba29ed596e34
    Reviewed-on: https://pigweed-review.googlesource.com/c/pigweed/sample_project/+/23440
    Reviewed-by: Anthony DiGirolamo <tonymd@google.com>
    Reviewed-by: Keir Mierle <keir@google.com>
    Commit-Queue: Armando Montanez <amontanez@google.com>

--- workshop/01-blinky/main.cc
+++ workshop/01-blinky/main.cc
@@ -1,18 +1,28 @@
-#include <Arduino.h>
-
+#include "pw_board_led/led.h"
 #include "pw_log/log.h"
 
-int main() {
-  constexpr int kLedPin = 13;
-  pinMode(kLedPin, OUTPUT);
+namespace {
 
+// This is a bad spin delay. It's not accurate, and doesn't correspond with any
+// real time unit. pw_chrono is coming soon, and will make it easier to do this
+// correctly!
+void Delay(size_t ticks) {
+  for (volatile size_t i = 0; i < ticks; i++) {
+    // Do nothing.
+  }
+}
+
+}  // namespace
+
+int main() {
+  pw::board_led::Init();
   while (true) {
     PW_LOG_INFO("Blink High!");
-    digitalWrite(kLedPin, HIGH);
-    delay(1000);
+    pw::board_led::TurnOn();
+    Delay(1000000);
     PW_LOG_INFO("Blink Low!");
-    digitalWrite(kLedPin, LOW);
-    delay(1000);
+    pw::board_led::TurnOff();
+    Delay(1000000);
   }
 
   return 0;
