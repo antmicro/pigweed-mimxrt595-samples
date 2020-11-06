1fd3319ab604de43d3e7dbe6a6355d9cbe79568d
Anthony DiGirolamo <tonymd@google.com>
2020-11-06 11:01:31 -0800

    pw find-file plugin
    Adds a find-file utility. Can find files based on glob patterns and
    allow running a command on each found file.
    
    Example invocation that finds elf files and runs them through the
    `arduino_unit_test_runner` command:
    
    pw find-files --starting-dir out/arduino_debug/
      --pattern '**/*.elf'
      --
      arduino_unit_test_runner --verbose --upload-tool teensyloader
      --config-file out/arduino_debug/gen/arduino_builder_config.json
      %f
    
    This could be useful for windows users (no unix find command) and
    serve an an example of how to add your own pw plugin script.
    
    usage: pw find-files [-h] [-l LOGLEVEL] [-s STARTING_DIR]
                         [--type {d,f}] -p PATTERN
                         ...
    
    File finding utility.
    
    positional arguments:
      -- EXEC_ARGS          Run a command on each file. The command should
                            be specified at the end after a '--'. Any args
                            matching '%f' will be replaced with the file
                            name.
    
    optional arguments:
      -h, --help            show this help message and exit
      -l LOGLEVEL, --loglevel LOGLEVEL
                            Set the log level (debug, info, warning, error,
                            critical)
      -s STARTING_DIR, --starting-dir STARTING_DIR
                            The starting directory to run a find. Default:
                            /home/tonymd/pw/pigweed
      --type {d,f}          Limit results to directories 'd' or files 'f'.
      -p PATTERN, --pattern PATTERN
                            Glob patterns to search with. Wildcard is '*'.
                            The '**' pattern means 'this directory and all
                            subdirectories, recursively'. Multiple patterns
                            can be used and are combined with a logical or.
    
    Change-Id: Ie5911a2c3995ca52dccb1bec4cc8b9c9363cb982
    Reviewed-on: https://pigweed-review.googlesource.com/c/pigweed/sample_project/+/23940
    Reviewed-by: Keir Mierle <keir@google.com>
    Commit-Queue: Anthony DiGirolamo <tonymd@google.com>

--- tools/setup.py
+++ tools/setup.py
@@ -15,12 +15,19 @@
 
 import setuptools  # type: ignore
 
-setuptools.setup(
-    name='sample_project_tools',
-    version='0.0.1',
-    description=__doc__,
-    packages=setuptools.find_packages(),
-    install_requires=[],
-    package_data={'sample_project_tools': ['py.typed']},
-    zip_safe=False,
-)
+setuptools.setup(name='sample_project_tools',
+                 version='0.0.1',
+                 author='Pigweed Authors',
+                 author_email='pigweed-developers@googlegroups.com',
+                 description=__doc__,
+                 packages=setuptools.find_packages(),
+                 package_data={'sample_project_tools': ['py.typed']},
+                 zip_safe=False,
+                 entry_points={
+                     'console_scripts': [
+                         'find-files = sample_project_tools.find_files:main',
+                     ]
+                 },
+                 install_requires=[
+                     'pw_cli',
+                 ])
