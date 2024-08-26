f4430f9de4274152c8e066a780f876d580099733
Łukasz Kędziora <lkedziora@antmicro.com>
2024-08-26 14:43:10 +0200

    [#64529] Add fastboot packet consumer loop

--- examples/02_usb_fastboot/main.cc
+++ examples/02_usb_fastboot/main.cc
@@ -1,38 +1,25 @@
-extern "C" {
-#include "usb_device_config.h"
-#include "usb.h"
-#include "usb_device.h"
-
-#include "usb_device_class.h"
-#include "usb_device_cdc_acm.h"
-#include "usb_device_ch9.h"
-
-#include "usb_device_descriptor.h"
-#include "virtual_com.h"
-}
-
-#include "fastboot.h"
-#include "pw_thread/thread.h"
+#include "transport_mimxrt595evk.h"
+#include "pw_log/log.h"
 #include "pw_thread/detached_thread.h"
-#include "pw_thread_freertos/context.h"
-#include "pw_thread_freertos/options.h"
 
-#define APP_TASK_STACK_SIZE 5000L
+static void FastbootProtocolLoop() {
+    static std::byte s_packet_buffer[512];
+    static constexpr char FASTBOOT_RESPONSE_OKAY[] = "OKAY";
 
-extern usb_cdc_vcom_struct_t s_cdcVcom;
-static pw::thread::freertos::StaticContextWithStack<APP_TASK_STACK_SIZE / sizeof(portSTACK_TYPE)> s_thread_static_ctx = {};
-static pw::thread::freertos::Options s_thread_opts = pw::thread::freertos::Options()
-    .set_name("usb_cdc_vcom")
-    .set_priority(4)
-    .set_stack_size(APP_TASK_STACK_SIZE / sizeof(portSTACK_TYPE))
-    .set_static_context(s_thread_static_ctx);
+    fastboot::mimxrt595evk::UsbTransportInit();
+    while (true) {
+        auto n = fastboot::mimxrt595evk::FastbootReceivePacket(pw::ByteSpan{s_packet_buffer, sizeof(s_packet_buffer)});
+        (void)n;
+        fastboot::mimxrt595evk::FastbootSendPacket(pw::ConstByteSpan{(std::byte const*)FASTBOOT_RESPONSE_OKAY, sizeof(FASTBOOT_RESPONSE_OKAY)});
+    }
+}
 
 namespace pw::system {
 
 void UserAppInit() {
-    pw::thread::DetachedThread(s_thread_opts, [] {
-        APPTask(&s_cdcVcom);
-    });
+    // Start new thread as WorkQueue thread stack is limited to 512.
+    pw::thread::DetachedThread(pw::thread::freertos::Options()
+        .set_name("fastboot"), FastbootProtocolLoop);
 }
 
 }  // namespace pw::system
