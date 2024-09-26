0d7294de67aab8854111cca7b3bd84518329f37c
Łukasz Kędziora <lkedziora@antmicro.com>
2024-09-26 10:45:40 +0200

    [#66320] 02_usb_fastboot: Read SW1 to enable fastboot mode

--- examples/02_usb_fastboot/main.cc
+++ examples/02_usb_fastboot/main.cc
@@ -1,42 +1,87 @@
-#include "pw_fastboot/device_hal.h"
-#include "pw_fastboot/device_variable.h"
-#include "pw_fastboot/transport.h"
-#include "pw_fastboot_usb_mcuxpresso/transport.h"
-#include "pw_thread/detached_thread.h"
-#include "pw_fastboot/fastboot_device.h"
 #include <cstdio>
 #include <memory>
 #include <string>
 
+#include "board.h"
+#include "pw_digital_io/digital_io.h"
+#include "pw_digital_io_mcuxpresso/digital_io.h"
+#include "pw_fastboot/device_hal.h"
+#include "pw_fastboot/device_variable.h"
+#include "pw_fastboot/fastboot_device.h"
+#include "pw_fastboot_usb_mcuxpresso/transport.h"
+#include "pw_log/log.h"
+#include "pw_status/status.h"
+#include "pw_thread/detached_thread.h"
+
+static bool IsGpioLow(pw::digital_io::DigitalIn& pin) {
+  if (const auto err = pin.Enable(); err != PW_STATUS_OK) {
+    PW_LOG_ERROR("Failed to enable GPIO with status: %d", err.code());
+    return false;
+  }
+
+  const auto gpio = pin.GetState()
+                        .or_else([](pw::Status s) {
+                          PW_LOG_ERROR("Failed GPIO with status: %d", s.code());
+                          return pw::digital_io::State::kInactive;
+                        })
+                        .value();
+
+  return (gpio == pw::digital_io::State::kInactive);
+}
+
+static bool IsFastbootTriggered() {
+  pw::digital_io::McuxpressoDigitalIn in{
+      BOARD_SW1_GPIO, BOARD_SW1_GPIO_PORT, BOARD_SW1_GPIO_PIN};
+  return IsGpioLow(in);
+}
+
+[[noreturn]] static void JumpToUser() {
+  PW_LOG_WARN("UNIMPLEMENTED: Bootloader would jump to user application now!");
+  PW_LOG_WARN("Hold SW1 / P0_25 GPIO low to boot into fastboot.");
+  while (true)
+    ;
+}
+
 static void FastbootProtocolLoop() {
-    auto variables = std::make_unique<pw::fastboot::VariableProvider>();
-    variables->RegisterVariable("test1", [](auto, auto, std::string* message) -> bool {
-        *message = "VARIABLE1";
-        return true;
-    });
-    variables->RegisterVariable("test2", [](auto, auto, std::string* message) -> bool {
-        *message = "TEST-VARIABLE-2";
-        return true;
-    });
-    variables->RegisterSpecialVariable("test3", [](pw::fastboot::Device* device) -> bool {
-        device->WriteInfo("This is an example of a special/multiline fastboot variable.");
+  auto variables = std::make_unique<pw::fastboot::VariableProvider>();
+  variables->RegisterVariable("test1",
+                              [](auto, auto, std::string* message) -> bool {
+                                *message = "VARIABLE1";
+                                return true;
+                              });
+  variables->RegisterVariable("test2",
+                              [](auto, auto, std::string* message) -> bool {
+                                *message = "TEST-VARIABLE-2";
+                                return true;
+                              });
+  variables->RegisterSpecialVariable(
+      "test3", [](pw::fastboot::Device* device) -> bool {
+        device->WriteInfo(
+            "This is an example of a special/multiline fastboot variable.");
         device->WriteInfo("This can be used for example to dump logs.");
         device->WriteInfo("To write a line, call device->WriteInfo.");
-        device->WriteInfo("Special variables are not displayed in output of fastboot getvar all.");
+        device->WriteInfo(
+            "Special variables are not displayed in output of fastboot getvar "
+            "all.");
         return true;
-    });
-    pw::fastboot::Device device {pw::fastboot::CreateMimxrt595UsbTransport(),
-                           std::move(variables),
-                           std::make_unique<pw::fastboot::DeviceHAL>()};
-    device.ExecuteCommands();
+      });
+  pw::fastboot::Device device{pw::fastboot::CreateMimxrt595UsbTransport(),
+                              std::move(variables),
+                              std::make_unique<pw::fastboot::DeviceHAL>()};
+  device.ExecuteCommands();
 }
 
 namespace pw::system {
 
 void UserAppInit() {
+  if (IsFastbootTriggered()) {
     // Start new thread as WorkQueue thread stack is limited to 512.
-    pw::thread::DetachedThread(pw::thread::freertos::Options()
-        .set_name("fastboot"), FastbootProtocolLoop);
+    pw::thread::DetachedThread(
+        pw::thread::freertos::Options().set_name("fastboot"),
+        FastbootProtocolLoop);
+    return;
+  }
+  JumpToUser();
 }
 
 }  // namespace pw::system
