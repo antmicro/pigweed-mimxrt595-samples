3542e17516473752a5326e1dec9dce8fac300831
Łukasz Kędziora <lkedziora@antmicro.com>
2024-10-21 11:54:23 +0200

    [#67459] 03_user_hello: Demo reboot into bootloader after 4 seconds

--- examples/03_user_hello/main.cc
+++ examples/03_user_hello/main.cc
@@ -1,10 +1,17 @@
+#include "board.h"
+#include "bootloader.h"
 #include "pw_log/log.h"
+#include "pw_thread/sleep.h"
 
 namespace pw::system {
 
 void UserAppInit() {
   PW_LOG_INFO("Hello world from user app!");
   PW_LOG_INFO("This should be booted from an alternative flash location.");
+  PW_LOG_INFO("Rebooting into fastboot in 4 seconds...");
+  pw::this_thread::sleep_for(std::chrono::seconds(4));
+  BOOTDATA->set(BootMode::Fastboot);
+  NVIC_SystemReset();
 }
 
 }  // namespace pw::system
